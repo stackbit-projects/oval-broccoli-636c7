5343b89a6ccdeb6df44d784ee605b83315181dbb
Stackbit Code Editor <projects@stackbit.com>
2021-11-18 11:32:54 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ export default class Footer extends React.Component {
                                     'justify-md-center': logo
                                 })}
                             >
-                                {logo && <Link className="site-footer__logo cell-12 cell-md-5 my-4" href={withPrefix('/')}><img src={withPrefix(logo)} alt={logoAlt} /></Link>}
+                                {logo && <Link className="site-footer__logo cell-12 cell-md-5 my-4" href={withPrefix('/')}><img  src={withPrefix(logo)} alt={logoAlt} /></Link>}
                                 {hasPrimaryNav && !_.isEmpty(primaryNavLinks) && this.renderNav(primaryNavLinks, primaryNavTitle)}
                                 {hasSecondaryNav && !_.isEmpty(secondaryNavLinks) && this.renderNav(secondaryNavLinks, secondaryNavTitle)}
                                 {hasTertiaryNav && !_.isEmpty(tertiaryNavLinks) && this.renderNav(tertiaryNavLinks, tertiaryNavTitle)}
